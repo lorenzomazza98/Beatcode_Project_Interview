8c0e80ba523c03a762a47aa152c4286f1a7a99a1
Lorenzo <74237081+lorenzomazza98@users.noreply.github.com>
2025-06-30 09:47:02 +0200

    Initial Project Setup

---
+++ Beatcode_Interview_Project/Beatcode_Interview_Project/Beatcode_Interview_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Beatcode_Interview_ProjectApp.swift
+//  Beatcode_Interview_Project
+//
+//  Created by Lorenzo Mazza on 30/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Beatcode_Interview_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
